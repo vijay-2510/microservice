2fd51afcbc3c1c2b66fff235608c3ae1c4ebf865
Vijay Pandey <kumarvijay2510@gmail.com>
2021-07-21 22:47:13 +0530

    Added limit service basic implementation

---
+++ src/main/java/com/vijay/microservices/limitsservice/controlle/LimitsController.java
@@ -0,0 +1,20 @@
+package com.vijay.microservices.limitsservice.controlle;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vijay.microservices.limitsservice.bean.Limits;
+import com.vijay.microservices.limitsservice.configuration.Configuration;
+
+@RestController
+public class LimitsController {
+
+	@Autowired
+	private Configuration configuration;
+
+	@GetMapping("/limits")
+	public Limits retrieveLimits() {
+		return new Limits(configuration.getMinimum(), configuration.getMaximum());
+	}
+}
